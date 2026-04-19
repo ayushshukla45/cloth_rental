a19f4f0f3b1f569f0035ee801bd2b9fc5ef49e5d
OyeLurker <oyelurker@gmail.com>
2026-04-20 00:02:55 +0530

    Order schema with userId, items, startDate, endDate, status

---
+++ backend/models/Order.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    products: [{
+        productId: {
+            type: Number,
+            required: true,
+        },
+        name: String,
+        image: String,
+        quantity: {
+            type: Number,
+            default: 1
+        },
+        startDate: {
+            type: Date,
+            required: true,
+        },
+        endDate: {
+            type: Date,
+            required: true,
+        },
+        pricePerDay: Number,
+    }],
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    paymentStatus: {
+        type: String,
+        enum: ["Pending", "Paid", "Failed"],
+        default: "Pending",
+    },
+    orderStatus: {
+        type: String,
+        enum: ["Booked", "Out for Delivery", "Rented", "Returned", "Cancelled"],
+        default: "Booked",
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
